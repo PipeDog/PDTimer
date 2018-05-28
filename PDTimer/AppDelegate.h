ec8c46e2809ed7c8f36cd9308f314dfce26b0e9b
liang <leiliang@didichuxing.com>
2018-05-28 20:17:35 +0800

    Initial Commit

---
+++ PDTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDTimer
+//
+//  Created by liang on 2018/5/28.
+//  Copyright © 2018年 PipeDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDTimer
+//
+//  Created by liang on 2018/5/28.
+//  Copyright © 2018年 PipeDog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
